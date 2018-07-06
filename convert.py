80eb8895f5b8961f51042ed0826b3601296ef915
Volker Krebs <volker.krebs@abas.de>
2018-07-06 12:00:30 +0200

    Notes and encoding

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+import io as io
 
 def createXML( values, activities ):
     date = values[0]
@@ -12,7 +13,7 @@ def createXML( values, activities ):
     # pulse = values[3]
     # weight = values[4]
     # weather = values[5]
-    # notes = values[6]
+    notes = values[6].strip()
     # shoe  = values[7]
     activity = ET.SubElement(activities, "Activity", Sport="Running")
     id = ET.SubElement(activity, "Id")
@@ -22,9 +23,12 @@ def createXML( values, activities ):
     tts.text = seconds
     dm = ET.SubElement(lap, "DistanceMeters")
     dm.text = length
+    if notes:
+      notesEle = ET.SubElement(activity, "Notes")
+      notesEle.text = notes
 
 #date,length,seconds,pulse,weight,weather,notes,shoe 
-f = open('Laufergebnisse.txt','r')
+f = io.open('Laufergebnisse.txt','r', encoding='iso-8859-1')
 
 root = ET.Element('TrainingCenterDatabase')
 activities = ET.SubElement(root, "Activities")
@@ -35,7 +39,7 @@ for line in f:
     
 f.close()
 
-t = minidom.parseString(ET.tostring(root)).toprettyxml() # Since ElementTree write() has no pretty printing support, used minidom to beautify the xml.
+t = minidom.parseString(ET.tostring(root)).toprettyxml(encoding="iso-8859-1") # Since ElementTree write() has no pretty printing support, used minidom to beautify the xml.
 tree = ET.ElementTree(ET.fromstring(t))
 
-tree.write("MyRun.tcx")
+tree.write("MyRun.tcx", encoding="iso-8859-1")
